62f088dd5676227634c1c687108db34f505e0321
Going9 <ickdor123@gmail.com>
2026-02-03 15:37:37 +0900

    feat: 전역 예외 처리 시스템 구축

---
+++ src/main/java/com/mindlog/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.mindlog.global.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 존재하는 데이터입니다.");
+    }
+}
