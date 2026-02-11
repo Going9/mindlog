713f6fe34c8887ffe7ca4976228929ffb9ac6b74
Going9 <ickdor123@gmail.com>
2026-02-12 00:25:34 +0900

    fix: Redis 캐시 및 handover 직렬화 오류 수정

--- src/main/java/com/mindlog/global/config/CacheConfig.java
+++ src/main/java/com/mindlog/global/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.mindlog.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import java.util.Map;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,23 +9,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+        var valueSerializer = RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer(objectMapper)
+        );
+
         var defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(valueSerializer)
                 .disableCachingNullValues();
 
         var cacheConfigs = Map.of(
                 "emotionAnalysis",
                 RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeValuesWith(valueSerializer)
                         .entryTtl(Duration.ofSeconds(90))
                         .disableCachingNullValues(),
                 "monthlyDiaries",
                 RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeValuesWith(valueSerializer)
                         .entryTtl(Duration.ofSeconds(60))
                         .disableCachingNullValues()
         );
